939c72d6a2cc5ecc728d70b24e5e4aeeffd496e7
TimurVs <vos-tim@yandex.ru>
2020-11-18 11:44:36 +0300

    homework lesson-1

---
+++ script.js
@@ -0,0 +1,31 @@
+var tempC = 21;
+var tempF = 9 / 5 * tempC + 32;
+
+var admin, name;
+
+name = "Василий";
+admin = name;
+
+alert(tempF);
+alert(name);
+
+//1000+108 будет равно 1000108
+
+//async и defer - посмотрел. порядок загрузки скриптов. понятно.
+
+
+// если я правильно понял задание, то:
+
+var a = 10, b = 20;
+a = a + 10;
+b = b - 10;
+
+alert(a);
+alert(b);
+
+
+
+
+
+
+
